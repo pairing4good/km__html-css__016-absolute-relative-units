61fad9403a9f41daf5b67e4dec41c5389ccb1a1d
pairing4good <pairing4good@gmail.com>
2022-11-02 08:03:43 -0500

    updated tests and readme

--- test/verify.test.js
+++ test/verify.test.js
@@ -36,90 +36,35 @@ afterEach(async () => {
   await browser.close();
 });
 
-describe("the narrow-text-area class", () => {
-  it("should be 100px wide", async () => {
-    const width = await page.$eval('div[class="narrow-text-area"]', (div) => {
-      let style = window.getComputedStyle(div);
-      return style.getPropertyValue("width");
+describe('the absolute-units div', () => {
+    it('should have an absolute margin value in pixels', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/#absolute-units.*{[\s\S][^}]*margin:.*px;/g).length;
+      });
+      expect(variableDefinitionCount).toEqual(1);
     });
-
-    expect(width).toEqual("100px");
-  });
-
-  it("should be centered on the page", async () => {
-    const autoMargins = await page.$eval(
-      'div[class="narrow-text-area"]',
-      (div) => {
-        let style = window.getComputedStyle(div);
-        return style.getPropertyValue("margin");
-      }
-    );
-
-    expect(autoMargins).toMatch(/0px \d\d\dpx/);
-  });
-
-  it("should be outlined with an orange border", async () => {
-    const borderColor = await page.$eval(
-      'div[class="narrow-text-area"]',
-      (div) => {
-        let style = window.getComputedStyle(div);
-        return style.getPropertyValue("border-color");
-      }
-    );
-
-    expect(borderColor).toEqual("rgb(255, 165, 0)");
-  });
-
-  it("should be outlined with a 5px wide border", async () => {
-    const borderWidth = await page.$eval(
-      'div[class="narrow-text-area"]',
-      (div) => {
-        let style = window.getComputedStyle(div);
-        return style.getPropertyValue("border-width");
-      }
-    );
-
-    expect(borderWidth).toEqual("5px");
-  });
-
-  it("should be outlined with a solid border", async () => {
-    const borderStyle = await page.$eval(
-      'div[class="narrow-text-area"]',
-      (div) => {
-        let style = window.getComputedStyle(div);
-        return style.getPropertyValue("border-style");
-      }
-    );
-
-    expect(borderStyle).toEqual("solid");
-  });
+    
+    it('should have an absolute padding value in pixels', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/#absolute-units.*{[\s\S][^}]*padding:.*px;[\s\S]*#relative-units/g).length;
+      });
+      expect(variableDefinitionCount).toEqual(1);
+    });
 });
 
-describe('the text-box class', () => {
-  it('should be backed with the color cyan behind the text', async () => {
-    const backgroundColor = await page.$eval('div[class="text-box"]', (div) => {
-      let style = window.getComputedStyle(div);
-      return style.getPropertyValue('background-color');
+describe('the relative-units div', () => {
+    it('should have margin values relative to the font-size of the element', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/#relative-units.*{[\s\S][^}]*margin:.*em;/g).length;
+      });
+      expect(variableDefinitionCount).toEqual(1);
     });
-
-    expect(backgroundColor).toEqual('rgb(0, 255, 255)');
-  });
-
-  it('should be padding the text with 5px on all sides', async () => {
-    const padding = await page.$eval('div[class="text-box"]', (div) => {
-      let style = window.getComputedStyle(div);
-      return style.getPropertyValue('padding');
+    
+    it('should have padding values relative to the font-size of the element', async () => {
+      const variableDefinitionCount = await page.$eval('style', (style) => {
+        return style.innerHTML.match(/#relative-units.*{[\s\S][^}]*padding:.*em;/g).length;
+      });
+      expect(variableDefinitionCount).toEqual(1);
     });
-
-    expect(padding).toEqual('5px');
-  });
-
-  it('should be surrounded with a margin of 10px on the top and bottom and 20px on the left and right', async () => {
-    const margin = await page.$eval('div[class="text-box"]', (div) => {
-      let style = window.getComputedStyle(div);
-      return style.getPropertyValue('margin');
-    });
-
-    expect(margin).toEqual('10px 20px');
-  });
 });
+
